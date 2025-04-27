cb75550bbe3017013b08d5eca60b88e34b4d881f
13CrysM <crysmupe@gamil.com>
2025-04-26 18:23:39 -0600

    Agregar archivos de proyecto.

---
+++ ClickAndEat/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using ClickAndEat.View;
+
+namespace ClickAndEat
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var loginView = new Login();
+            loginView.Show(); //Show para mostrar la ventana
+            loginView.IsVisibleChanged += (s, v) =>
+            {
+                if (loginView.IsVisible == false && loginView.IsLoaded)
+                {
+                    var mainView = new MainWindow();
+                    mainView.Show();
+                    loginView.Close();
+                }
+            };
+        }
+    }
+}
